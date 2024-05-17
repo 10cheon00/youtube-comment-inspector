0f40443e7d163faa9e9c6a7ef22e2afb692452c3
10cheon00 <10cheon00@gmail.com>
2024-05-18 01:20:23 +0900

    remove test annotation

--- src/test/java/com/hackertonhuru/youtubecommentinspector/YoutubeCommentInspectorApplicationTests.java
+++ src/test/java/com/hackertonhuru/youtubecommentinspector/YoutubeCommentInspectorApplicationTests.java
@@ -3,7 +3,6 @@ package com.hackertonhuru.youtubecommentinspector;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class YoutubeCommentInspectorApplicationTests {
 
 	@Test
