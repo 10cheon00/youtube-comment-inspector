7ee0eb88347e28df516c73639b073ce1a8cdec0f
10cheon00 <10cheon00@gmail.com>
2024-05-18 03:22:29 +0900

    fix: allow all in cors

--- src/main/java/com/hackertonhuru/youtubecommentinspector/controller/YoutubeLinkController.java
+++ src/main/java/com/hackertonhuru/youtubecommentinspector/controller/YoutubeLinkController.java
@@ -3,15 +3,13 @@ package com.hackertonhuru.youtubecommentinspector.controller;
 import com.hackertonhuru.youtubecommentinspector.util.YoutubeUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/")
 public class YoutubeLinkController {
     private final YoutubeUtil youtubeUtil;
